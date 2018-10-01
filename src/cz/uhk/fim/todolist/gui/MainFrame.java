89713d1a2642ecce9aebf625b587e35a81420465
kamenmi1 <miloslav.kamenik@outlook.cz>
2018-10-01 14:29:39 +0200

    Todolist

--- src/cz/uhk/fim/todolist/gui/MainFrame.java
+++ src/cz/uhk/fim/todolist/gui/MainFrame.java
@@ -1,6 +1,8 @@
 package cz.uhk.fim.todolist.gui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
 
@@ -13,5 +15,23 @@ public class MainFrame extends JFrame {
         setSize(500,500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        initControlPanel();
     }
+
+    private void initControlPanel() {
+        JPanel controlPanel = new JPanel(new BorderLayout());
+
+        JLabel lbladdTodo = new JLabel("Zadej todos:");
+        JTextField txtAddTodo = new JTextField();
+        JButton btnAdd = new JButton("Přidat");
+
+        controlPanel.add(lbladdTodo, BorderLayout.WEST);
+        controlPanel.add(txtAddTodo, BorderLayout.CENTER);
+        controlPanel.add(btnAdd, BorderLayout.EAST);
+
+        add(controlPanel, BorderLayout.NORTH);
+
+    }
+
 }
